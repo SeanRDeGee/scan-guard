edb96c158fcf66d4903b4ad699cebb49eed4e54d
Sean De Gee <sean.de.gee@sintrex.com>
2024-05-18 12:47:32 +0200

    added default page metadata

--- apps/scan-guard/nuxt.config.ts
+++ apps/scan-guard/nuxt.config.ts
@@ -1,6 +1,18 @@
 export default defineNuxtConfig({
     ssr: false,
     modules: ['nuxt-svgo'],
+    app: {
+        head: {
+            viewport: 'width=device-width, initial-scale=1',
+            charset: 'utf-8',
+            title: 'Sintrex ScanGaurd',
+            meta: [{ hid: 'description', name: 'description', content: 'Sintrex ScanGaurd' }],
+            link: [{ rel: 'icon', href: '/favicon.svg' }],
+            htmlAttrs: {
+                lang: 'en',
+            },
+        },
+    },
     svgo: {
         componentPrefix: 'svg',
         autoImportPath: './assets/svg/',
