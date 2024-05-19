e665a2089d60c187cfd56b2806af036f2a0aeb8e
Sean De Gee <sean.de.gee@sintrex.com>
2024-05-19 10:47:43 +0200

    home header

--- apps/scan-guard/pages/app/index.vue
+++ apps/scan-guard/pages/app/index.vue
@@ -1,7 +1,30 @@
-<script lang="ts" setup>
-    const requestStore = useRequestsStore()
-</script>
-
 <template>
-    <div class="app-home"></div>
+    <div class="app-home">
+        <header class="app-home__header">
+            <h1 class="app-home__header__title">ScanGuard Tools</h1>
+        </header>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+    .app-home {
+        padding: 1.5rem;
+
+        align-content: start;
+        display: grid;
+        row-gap: 1.5rem;
+
+        animation: var(--anim-drop-appear);
+
+        &__header {
+            &__title {
+                padding-bottom: 1rem;
+
+                border-bottom: 1px solid var(--clr-border-primary);
+                color: var(--clr-text-secondary);
+
+                font-size: 1.5rem;
+            }
+        }
+    }
+</style>
